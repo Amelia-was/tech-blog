2d320bfb5ea144133b133f46cfd72ffa5f741e6b
Deisel <74831846+Amelia-was@users.noreply.github.com>
2021-03-06 16:46:01 -0500

    user get/post routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -11,4 +11,38 @@ router.get('/', (req, res) => {
     });
 });
 
+// get one user by id
+router.get('/:id', (req,res) => {
+    User.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// create new user
+router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+    .then(dbUserData => {
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// update username
+
+// delete user by id
+
 module.exports = router;
